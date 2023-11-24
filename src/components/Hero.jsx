83b21e5fd98f600bbfb39b24a09da84e7efbe408
daniel-kav <dankavatha01@gmail.com>
2023-11-24 15:28:17 +0300

    navbar done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+
 
 const Hero = () => {
   return (
