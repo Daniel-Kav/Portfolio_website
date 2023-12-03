e6468615c0da713f9cdbd52a78772e4d927132b4
daniel-kav <dankavatha01@gmail.com>
2023-12-03 15:25:30 +0300

    about section done

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
 
-const SectionWrapper = () => 
+const SectionWrapper = ( Component, idName) => 
 function HOC(){
     return (
         <motion.section
@@ -13,7 +14,10 @@ function HOC(){
            viewport={{ once: true, amount: 0.25}}
            className={`${styles.padding} max-w-7xl mx-auto relative z-0`} 
         >
-            <component />
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
+            <Component />
         </motion.section>
     )
 }
