70dbc041bbf6d50642569a4645c4f0a4d12b4fea
daniel-kav <dankavatha01@gmail.com>
2023-12-09 00:42:16 +0300

    creating a stars canvas that renders behind the contact page

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as random from 'math/random/dist/math-random.esm';
+import { StarsCanvas } from '.';
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+
+const StarsCanvas = () => {
+
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1]'>
+      <Canvas camera={{ position: [0, 0, 1]}}>
+        <Suspense fallback={null} >
+          <Stars />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
+}
+export default StarsCanvas;
